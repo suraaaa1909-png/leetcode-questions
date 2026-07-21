9e1daecbb503807b4c25d7b88315ce612b1874d0
suraaaa1909-png <suraaaa1909@gmail.com>
2026-07-21 10:45:53 +0530

    Update day 2-tow sum.java

--- day 2-tow sum.java
+++ day 2-tow sum.java
@@ -1,22 +1,13 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap[nums[i]] = i;
-        }
-
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement) && numMap[complement] != i) {
-                return {i, numMap[complement]};
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
-
-        return {}; // No solution found
+        return new int[]{}; // No solution found
     }
-};
+}
